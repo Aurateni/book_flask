805f473b655741f9399e2c97b91582391be69e8e
Arsenii <aurateni@gmail.com>
2022-09-13 13:38:26 +0300

    Add Forms

--- reader/routes.py
+++ reader/routes.py
@@ -1,12 +1,12 @@
-from flask import render_template, send_from_directory
-
+from flask import render_template, send_from_directory, request
 from reader import app
 from reader.models import Book
 
 
 @app.route('/')
 def index():
-    books = Book.query.all()
+    page = request.args.get('page', 1, type=int)
+    books = Book.query.order_by(Book.created_at.desc()).paginate(page=page, per_page=4)
     return render_template('index.html', books=books)
 
 
@@ -23,11 +23,13 @@ def book(book_id):
 
 @app.route('/thrillers/')
 def thrillers():
-    books = Book.query.filter(Book.genre == 'триллер').all()
+    page = request.args.get('page', 1, type=int)
+    books = Book.query.filter(Book.genre == 'триллер').paginate(page=page, per_page=4)
     return render_template('thrillers.html', books=books)
 
 
 @app.route('/best/')
 def best():
-    books = Book.query.filter(Book.rating > 4).all()
+    page = request.args.get('page', 1, type=int)
+    books = Book.query.filter(Book.rating > 4).paginate(page=page, per_page=4)
     return render_template('best.html', books=books)
